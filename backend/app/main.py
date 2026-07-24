10d551a0feee71c0a66a799e1063825c6308022c
viralibanka <viralibanka@gmail.com>
2026-07-24 22:58:10 +0530

    Fix frontend CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://digital-expiry-assisstance-1.onrender.com/login.html"
+        "https://digital-expiry-assisstance-1.onrender.com",
         "http://127.0.0.1:5500",
         "http://localhost:5500"
     ],
